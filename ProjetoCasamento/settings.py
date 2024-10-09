ee3ffddd94020c5d9c669293617982e67c1098f1
mateusvega <mateusggvega@gmail.com>
2024-10-09 11:49:54 -0300

    commit

--- ProjetoCasamento/settings.py
+++ ProjetoCasamento/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(z#vdo1$&y=g78gqet#fms(8!&4a@pl$(@d58_%%on$swaasc4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
