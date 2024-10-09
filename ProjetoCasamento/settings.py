35db278da589344aa18bec30603d7b967c21946d
mateusvega <mateusggvega@gmail.com>
2024-10-09 12:12:48 -0300

    config again

--- ProjetoCasamento/settings.py
+++ ProjetoCasamento/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(z#vdo1$&y=g78gqet#fms(8!&4a@pl$(@d58_%%on$swaasc4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 
